3a92e25f45d4b70bfc17f2126f67c46726454e8c
theenduranceA <theenduranceaneke@gmail.com>
2024-01-03 20:59:15 +0000

    Task 5

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,15 +1,11 @@
-// uploadPhoto.js
-
 export default function uploadPhoto(fileName) {
   return new Promise((resolve, reject) => {
-    // Simulating an error condition (you can modify this based on your requirements)
     const errorCondition = true;
 
     if (errorCondition) {
       const errorMessage = `${fileName} cannot be processed`;
       reject(new Error(errorMessage));
     } else {
-      // Simulating a successful photo upload (you can modify this based on your requirements)
       const successMessage = `${fileName} has been successfully uploaded`;
       resolve(successMessage);
     }
