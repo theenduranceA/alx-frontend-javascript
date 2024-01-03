12c3fb399c14f25cd46b19cd6617f012af14218f
theenduranceA <theenduranceaneke@gmail.com>
2024-01-03 21:53:54 +0000

    Task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then(
+    (results) => results,
+    (error) => [{
+      status: 'rejected',
+      value: error,
+    }],
+  );
+}
