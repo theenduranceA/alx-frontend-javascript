5d903e3acabdc5082bedb0b653b27eaa4415b65d
theenduranceA <theenduranceaneke@gmail.com>
2024-01-03 15:09:44 +0000

    Task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,5 +2,10 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
+  if (trueOrFalse) {
+    var task = true;
+    var task2 = false;
+  }
+
   return [task, task2];
 }
