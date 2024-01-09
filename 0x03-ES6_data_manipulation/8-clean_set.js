9e0575168dbd1edc5b6a8083c23dc8476efa7301
theenduranceA <theenduranceaneke@gmail.com>
2024-01-09 17:41:52 +0000

    Task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,7 @@
 export default function cleanSet(set, startString) {
-  const myValues = Array.from(set).filter((value) => value.startsWith(startString));
+  const myValues = Array.from(set)
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.substring(startString.length));
   const myString = myValues.join('-');
 
   return myString;
