4fac2e2478c32750d6749d097d1e571f5889b7f6
theenduranceA <theenduranceaneke@gmail.com>
2024-01-09 12:56:26 +0000

    Task 1

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+export default function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  const ids = students.map((student) => student.id);
+  return ids;
+}
