1cc481882165738c3646ae3b8ac4301d870d6c96
theenduranceA <theenduranceaneke@gmail.com>
2024-01-09 20:45:35 +0000

    Task 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,8 +1,3 @@
-export default function getStudentIdsSum(students) {
-  if (!Array.isArray(students)) {
-    return [];
-  }
-
-  const sum = students.reduce((accumulator, student) => accumulator + student.id);
-  return sum;
+export default function getStudentIdsSum(list) {
+  return list.reduce((sum, student) => sum + student.id, 0);
 }
