85f156c84619c79e44af4b5a44220216e4ebd631
theenduranceA <theenduranceaneke@gmail.com>
2024-01-10 16:31:24 +0000

    Task 0

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,22 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 20,
+  location: "New York",
+};
+
+const student2: Student = {
+  firstName: "Jane",
+  lastName: "Doe",
+  age: 22,
+  location: "San Francisco",
+};
+
+const studentsList: Student[] = [student1, student2];
